34059dbfce90b952a7bdbb07d0fea2b8ec116911
Eduardo J. Brenes Perez <edubpz08@gmail.com>
2025-06-17 17:58:57 -0600

    Protecting routes based on authentication.

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,22 +1,53 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:teslo_shop/config/router/app_router_notifier.dart';
 import 'package:teslo_shop/features/auth/auth.dart';
 import 'package:teslo_shop/features/products/products.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/login',
-  routes: [
-    GoRoute(
-      path: '/login',
-      builder: (context, state) => const LoginScreen(),
-    ),
-    GoRoute(
-      path: '/register',
-      builder: (context, state) => const RegisterScreen(),
-    ),
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const ProductsScreen(),
-    ),
-  ],
-  ///! TODO: Denied routes when is authenticated.
-);
+final goRouterProvider = Provider((ref) {
+  final goRouterNotifier = ref.read(goRouterNotifierProvider);
+
+  return GoRouter(
+    initialLocation: '/check-status',
+    refreshListenable: goRouterNotifier,
+    routes: [
+      GoRoute(
+        path: '/check-status',
+        builder: (context, state) => const CheckAuthStatusScreen(),
+      ),
+
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        path: '/register',
+        builder: (context, state) => const RegisterScreen(),
+      ),
+      
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const ProductsScreen(),
+      ),
+    ],
+    
+    redirect: (context, state) {
+      final isGoingTo = state.subloc;
+      final authStatus = goRouterNotifier.authStatus;
+
+      if(isGoingTo == '/check-status' && authStatus == AuthStatus.checking) return null;
+
+      if(authStatus == AuthStatus.notAuthenticated) {
+        if(isGoingTo == '/login' || isGoingTo == '/register') return null;
+        
+        return '/login';
+      }
+
+      if(authStatus == AuthStatus.authenticated) {
+        if(isGoingTo == '/login' || isGoingTo == '/register' || isGoingTo == '/check-status') return '/';
+      }
+
+      return null;
+    },
+  );
+});
